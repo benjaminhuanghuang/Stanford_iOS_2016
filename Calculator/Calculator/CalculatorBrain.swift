1d4be123ffe84c0231f1bac6bf23576b5c4d1446
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-03-04 00:56:51 -0800

    update

--- Calculator/Calculator/CalculatorBrain.swift
+++ Calculator/Calculator/CalculatorBrain.swift
@@ -8,6 +8,10 @@
 
 import Foundation
 
+func multiply(op1: Double, op2:Double) -> Double{
+    return op1 * op2
+}
+
 internal class CalculatorBrain
 {
     private var accumulator:Double = 0.0
@@ -20,37 +24,49 @@ internal class CalculatorBrain
     func setOperand(operand: Double){
         accumulator = operand
     }
+    
+   
+
     var operations: Dictionary<String, Operation>=[
-        "π":Operation.Constant,//M_PI,
-        "e":Operation.Constant,//M_E,
-        "√":Operation.UnaryOperation,//sqrt,
-        "cos": Operation.UnaryOperation//cos,
+        "π":Operation.Constant(M_PI),
+        "e":Operation.Constant(M_E),
+        "√":Operation.UnaryOperation(sqrt),
+        "cos": Operation.UnaryOperation(cos),
+        "x": Operation.BinaryOperation(multiply),
+        "=": Operation.Equals
         
         
     ]
     
     enum Operation{
-        case Constant
-        case UnaryOperation
-        case BinaryOperation
+        case Constant(Double)
+        case UnaryOperation((Double)->Double)
+        case BinaryOperation((Double, Double)->Double)
         case Equals
     }
     
+    private var pending: PendingBinaryOperationInfo?
+    
+    struct PendingBinaryOperationInfo{
+        var binaryFunction:(Double, Double)->Double
+        var firstOperand: Double
+    }
     func performOperation(symbol: String)
     {
         //bypass the case can not orgnize the symbol
         if let option = operations[symbol]{
             switch option {
-            case .Constant:
-                break
-            case .UnaryOperation:
-                break
-            case .BinaryOperation:
-                break
+            case .Constant(let value):
+                accumulator = value
+            case .UnaryOperation(let fun):
+                accumulator = fun(accumulator)
+            case .BinaryOperation(let fun):
+                pending = PendingBinaryOperationInfo(binaryFunction:fun, firstOperand: accumulator)
             case .Equals:
-                break
-            default:
-                <#code#>
+                if pending != nil
+                {
+                    accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator)
+                }
             }
         }
 //        switch symbol {
